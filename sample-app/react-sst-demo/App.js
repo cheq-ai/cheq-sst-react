b3fc800108f9045fa0a92342c4c3aa865bcf81db
Anthony Durgavich <anthony.d@cheq.ai>
2026-01-30 19:10:59 +0000

    fix npx issues

--- sample-app/react-sst-demo/App.js
+++ sample-app/react-sst-demo/App.js
@@ -5,6 +5,7 @@ import { Sst, Config, Event } from "cheq-sst-react";
 
 export default function App() {
 	useEffect(() => {
+        console.log("useEffect");
         let cancelled = false;
 
         (async () => {
